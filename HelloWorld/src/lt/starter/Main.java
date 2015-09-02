af5cd8727993769606584a604ed6bf758693d2fd
Jonas <jonasjhost@gmail.com>
2015-09-02 16:08:33 +0300

    Labas

---
+++ HelloWorld/src/lt/starter/Main.java
@@ -0,0 +1,10 @@
+package lt.starter;
+
+/**
+ * Created by Jonas on 2015-09-02.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
