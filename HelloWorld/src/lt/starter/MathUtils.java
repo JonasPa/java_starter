0bc542a8a7aec67d8ef463854d321b073cd4e8a2
Tautvydas <Tautvydas@Tautvydas.com>
2015-09-09 14:36:16 +0300

    MathUtils assignment

---
+++ HelloWorld/src/lt/starter/MathUtils.java
@@ -0,0 +1,23 @@
+package lt.starter;
+
+/**
+ * Created by Tautvydas on 9/9/2015.
+ */
+public class MathUtils {
+
+    public static long sum(int... numbers){
+        return -1;
+    }
+
+    public static int max(int... numbers){
+        return -1;
+    }
+
+    public static int min(int... numbers){
+        return -1;
+    }
+
+    public static long multiply(int... numbers){
+        return -1;
+    }
+}
---
+++ HelloWorld/test/lt/starter/MathUtilsTest.java
@@ -0,0 +1,31 @@
+package lt.starter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Tautvydas on 9/9/2015.
+ */
+public class MathUtilsTest {
+
+    @Test
+    public void testSum() throws Exception {
+
+    }
+
+    @Test
+    public void testMax() throws Exception {
+
+    }
+
+    @Test
+    public void testMin() throws Exception {
+
+    }
+
+    @Test
+    public void testMultiply() throws Exception {
+
+    }
+}
