9eb4c11d0c9d61a52effd69634c5435f7e16c10e
Jonas <jonasjhost@gmail.com>
2015-09-22 18:43:48 +0300

    RollDice with snake eyes working

--- HelloWorld/src/lt/starter/RollTheDice.java
+++ HelloWorld/src/lt/starter/RollTheDice.java
@@ -1,16 +1,24 @@
 package lt.starter;
 
 public class RollTheDice {
-   public static void main(String[] args) {
-     int die1 = rollDice();
-     int die2 = rollDice();
-     int roll = die1 + die2;
+    public static void main(String[] args) {
 
-      System.out.println("Pirmas metimas = " + die1);
-      System.out.println("Antras metimas = " + die2);
-      System.out.println("Metimu suma = " + roll);
-   }
-    private static int rollDice() {
-        return (int)(Math.random()*6) + 1;
+        int die1 = rollDice();
+        int die2 = rollDice();
+        int roll = die1 + die2;
+        int countRolls = 0;
+        while (isNotSnakeEyes()){
+            countRolls++;
+        }
+        System.out.println("It took " + countRolls + " rolls to get snake eyes.");
+    }
+
+    public static int rollDice() {
+        return (int) (Math.random() * 6) + 1;
+    }
+
+
+    public static boolean isNotSnakeEyes() {
+        return !(rollDice() == 1 && rollDice() == 1);
     }
 }
