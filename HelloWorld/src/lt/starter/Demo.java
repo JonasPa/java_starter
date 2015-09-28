85607dad751792b985cb164525b5b371c58dcc81
Jonas <Jonasjhost@gmail.com>
2015-09-28 18:46:00 +0300

    Replace Character to another with method

---
+++ HelloWorld/src/lt/starter/Demo.java
@@ -0,0 +1,14 @@
+package lt.starter;
+
+/**
+ * Created by Andrius-PC on 2015-09-28.
+ */
+class ArrayCopyDemo {
+    public static void main(String[] args) {
+        char[] copyFrom = { 'd', 'e', 'c', 'a', 'f', 'f', 'e',
+                'i', 'n', 'a', 't', 'e', 'd' };
+        for (int i = 0; i < copyFrom.length; i++)
+
+        System.out.println(copyFrom[i]);
+    }
+}
--- HelloWorld/src/lt/starter/ReplaceText.java
+++ HelloWorld/src/lt/starter/ReplaceText.java
@@ -1,20 +1,18 @@
 package lt.starter;
+
 /**
  * Created by Andrius-PC on 2015-09-28.
  */
 public class ReplaceText {
     public static String replaceChar(String text, char from, char to) {
-        String result = "";
-        char[] words = result.toCharArray();
+        char[] words = text.toCharArray();
+        // String letter = String.valueOf(words);
         for (int i = 0; i < text.length(); i++) {
             char symbol = text.charAt(i);
-        if (symbol == from){
-           result = result+to;
-            } else {
-            result = result+symbol;
-                }
-
+            if (symbol == from) {
+                words[i] = to;
             }
-            return result;
         }
+        return String.valueOf(words);
     }
+}
